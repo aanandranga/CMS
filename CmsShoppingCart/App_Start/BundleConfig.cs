063b29590ef32b736ae8fe1b7796743ca3156b7f
aanandranga <anand.ranga79@gmail.com>
2018-08-08 16:59:55 +0530

    after 6 video commit

--- CmsShoppingCart/App_Start/BundleConfig.cs
+++ CmsShoppingCart/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace CmsShoppingCart
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                       "~/Scripts/jquery-{version}.js",
+                       "~/Scripts/bootstrap.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
