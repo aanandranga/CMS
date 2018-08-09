e41d938e0c9db3b051a4cfe94db8f0bf65216590
aanandranga <anand.ranga79@gmail.com>
2018-08-09 20:45:44 +0530

    added database successfully
    08 - 02 done

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using CmsShoppingCart.Models.Data;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        //
+        // GET: /Admin/Pages/
+        public ActionResult Index()
+        {
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+            return View(pagesList);
+        }
+	}
+}
