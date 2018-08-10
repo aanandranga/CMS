38dbf145ac55aabf88aeba3193ba1b5bd71d3de2
aanandranga <anand.ranga79@gmail.com>
2018-08-10 16:42:29 +0530

    with editpage method

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -28,5 +28,67 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         {
             return View();
         }
+
+        // POST: /Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (Db db = new Db())
+            {
+                string slug;
+                PageDTO dto = new PageDTO();
+                dto.Title = model.Title;
+
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists.");
+                    return View(model);
+                }
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.Sidebar = model.Sidebar;
+                dto.Sorting = 100;
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "You have added a new page";
+
+            return RedirectToAction("AddPage");
+
+            
+        }
+
+        public ActionResult EditPage(int id)
+        {
+            PageVM model;
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+                model = new PageVM(dto);
+               
+            }
+            return View(model);
+        }
+      
 	}
 }
