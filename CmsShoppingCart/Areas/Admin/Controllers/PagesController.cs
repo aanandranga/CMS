6633266181a5eb8e51b92c77ae7e6ece905778c3
aanandranga <anand.ranga79@gmail.com>
2018-08-13 23:46:24 +0530

    step 15 done

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -74,6 +74,7 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             
         }
 
+        [HttpGet]
         public ActionResult EditPage(int id)
         {
             PageVM model;
@@ -89,6 +90,94 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             }
             return View(model);
         }
-      
+
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                int id = model.Id;
+                string slug="home";
+                PageDTO dto = db.Pages.Find(id);
+                dto.Title = model.Title;
+
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) || db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists.");
+                    return View(model);
+                }
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.Sidebar = model.Sidebar;
+                db.SaveChanges();
+
+                TempData["SM"] = "You have edited the page";
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult PageDetails(int id)
+        {
+            PageVM model;
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+                model = new PageVM(dto);
+            }
+            return View(model);
+        }
+
+        public ActionResult DeletePage(int id)
+        {
+            using(Db db=new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+                db.Pages.Remove(dto);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+                PageDTO dto;
+                foreach (var pageId in id)
+                {
+                    dto=db.Pages.Find(pageId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+
 	}
 }
