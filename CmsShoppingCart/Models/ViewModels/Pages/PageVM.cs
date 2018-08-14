2bce78797e1194eb11af1657548844268d74cff3
aanandranga <anand.ranga79@gmail.com>
2018-08-14 15:53:53 +0530

    last commit

--- CmsShoppingCart/Models/ViewModels/Pages/PageVM.cs
+++ CmsShoppingCart/Models/ViewModels/Pages/PageVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CmsShoppingCart.Models.ViewModels.Pages
 {
@@ -33,6 +34,8 @@ namespace CmsShoppingCart.Models.ViewModels.Pages
 
         [Required]
         [StringLength(int.MaxValue,MinimumLength=3)]
+
+        [AllowHtml]
         public string Body { get; set; }
         public int Sorting { get; set; }
         public bool Sidebar { get; set; }
